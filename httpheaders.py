8fc864af418d195e3001c21ad1f6bade473720ef
Marcin Orzepowski <marcin.orzepowski@nask.pl>
2019-01-18 08:45:58 +0100

    Abspath

--- httpheaders.py
+++ httpheaders.py
@@ -1,4 +1,5 @@
 import json
+import os
 import os.path
 from collections import OrderedDict
 
@@ -13,19 +14,21 @@ app = Flask(__name__)
 def dedup(data):
     return list(OrderedDict.fromkeys(data))
 
+
 @app.route('/hh')
 def httpheaders():
     get_request()
     if request.args.get("pretty") is None:
-        with open(HTTP_HEADERS_FILENAME, "r") as f:
-            data = f.read()       
+        with open(os.path.abspath(HTTP_HEADERS_FILENAME), "r") as f:
+            data = f.read()
         return "[" + data[:-2] + "]"
     else:
-        with open(HTTP_HEADERS_FILENAME, "r") as f:
+        with open(os.path.abspath(HTTP_HEADERS_FILENAME), "r") as f:
             data = f.readlines()
         # remove last colon
-        data[-1]=data[-1][:-2]
+        data[-1] = data[-1][:-2]
         return render_template("headers.html", headers=data)
+
 
 def get_request():
     # copy of headers
@@ -36,24 +39,25 @@ def get_request():
             headers.pop(h)
     json_headers = json.dumps(OrderedDict(headers))
     # create file if not exists
-    if not os.path.isfile(HTTP_HEADERS_FILENAME):
-        with open(HTTP_HEADERS_FILENAME, "w+"):
+    if not os.path.isfile(os.path.abspath(HTTP_HEADERS_FILENAME)):
+        with open(os.path.abspath(HTTP_HEADERS_FILENAME), "w+"):
             pass
 
     # read file
-    with open(HTTP_HEADERS_FILENAME, "r") as f:
+    with open(os.path.abspath(HTTP_HEADERS_FILENAME), "r") as f:
         headers_data = f.readlines()
     # append new headers
     headers_data.append(json_headers + ",\n")
     # deduplicate
     headers_data_to_write = dedup(headers_data)
-    
+
     # write output data
-    with open(HTTP_HEADERS_FILENAME, "w") as f:
+    with open(os.path.abspath(HTTP_HEADERS_FILENAME), "w") as f:
         for h in headers_data_to_write:
             f.write(h)
-    
+
     return json_headers
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -67,17 +71,18 @@ def ipaddr():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         return request.environ['REMOTE_ADDR']
     else:
-        return request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy    
-    
+        return request.environ['HTTP_X_FORWARDED_FOR']  # if behind a proxy
+
 
 @app.route('/ua')
 def ua():
     get_request()
     headers = request.headers
-    if "User-Agent" in headers:    
+    if "User-Agent" in headers:
         return headers["User-Agent"]
     else:
         return ""
+
 
 @app.route('/uagents')
 def uagents():
@@ -98,10 +103,12 @@ def uagents():
     else:
         return render_template("uagents.html", uagents=uagents)
 
+
 @app.route('/help')
 def helpinfo():
     get_request()
     return render_template("help.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
